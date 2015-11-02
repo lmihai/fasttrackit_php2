e50672db4a3daaaf303badccafe31638e5e0add5
rushoratiu <rushoratiu3@gmail.com>
2015-11-02 19:22:12 +0200

    ...

--- Horatiu/homework 4/slide19.php
+++ Horatiu/homework 4/slide19.php
@@ -20,10 +20,28 @@ for ($j=$start1; $j < $stop1 ; $j=$j+2) {
 	$sum1 = $sum1 + $j;
 }
 echo $sum1;
+echo "<p></p>";
+echo "<p></p>";
+
+//sum of all numbers from 1000 to 1;
+
+$start2 = 1000;
+$stop2 = 1;
+$sum2 = 0;
+for ($k=$start2; $k >$stop2 ; $k--) { 
+	$sum2 = $sum2 + $k;
+}
+echo $sum2;
 
 
-
-
+echo "<p></p>";
+$start3 = 1;
+$stop3 = 1000;
+$sum3 = 0;
+for ($x=$start3; $x <$stop3 ; $x++) { 
+	$sum3 = $sum3 + $x;
+}
+echo $sum3;
 
 
 
