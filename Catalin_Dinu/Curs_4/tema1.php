90b3379705e5a142d9ea077bc075e692926aac16
catalinradudinu <catalinradudinu@gmail.com>
2015-11-03 10:17:29 +0200

    1.1 - make associative array

--- Catalin_Dinu/Curs_4/tema1.php
+++ Catalin_Dinu/Curs_4/tema1.php
@@ -1,8 +1,8 @@
 <?php
 $loto_players = array(
-					array("John", "Doe", 11, 3, 22, 17,36, 40),
-					array("Jane", "Foo", 32, 6, 18, 21,13, 42),
-					array("Elvis", "Peanutbutter", 21, 7, 17, 35, 5, 44)
+					array("first_name" => "John", "last_name" => "Doe", "loto_ticket" => array(11, 3, 22, 17,36, 40)),
+					array("first_name" => "Jane", "last_name" => "Foo", "loto_ticket" => array(32, 6, 18, 21,13, 42)),
+					array("first_name" => "Elvis", "last_name" => "Peanutbutter", "loto_ticket" => array(21, 7, 17, 35, 5, 44))
 					);
 print_r($loto_players);
 ?>
