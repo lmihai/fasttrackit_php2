d9d9bf013e2978f5d60a3c26d33687a70073f8f0
rushoratiu <rushoratiu3@gmail.com>
2015-11-03 11:39:04 +0200

    While loop  -  slide 19

--- Horatiu/homework 4/slide19.php
+++ Horatiu/homework 4/slide19.php
@@ -12,23 +12,25 @@ for ($i=$start; $i <= $stop; $i = $i + 2) {
 echo $sum;
 echo "<p></p>";
 //sum of all odd numbers from 20 to 800
-$start1 = 19;
-$stop1 = 801;
+$start1 = 20;
+$stop1 = 800;
 $sum1 = 0;
 
-for ($j=$start1; $j < $stop1 ; $j=$j+2) { 
-	$sum1 = $sum1 + $j;
+for ($j=$start1; $j < $stop1 ; $j++) { 
+	if ($j %2 != 0) {
+		$sum1 = $sum1 + $j;
+	}
 }
 echo $sum1;
 echo "<p></p>";
 echo "<p></p>";
 
-//sum of all numbers from 1000 to 1;
+//sum of all numbers from 1 to 1000;
 
 $start2 = 1000;
 $stop2 = 1;
 $sum2 = 0;
-for ($k=$start2; $k >$stop2 ; $k--) { 
+for ($k=$start2; $k >=$stop2 ; $k--) { 
 	$sum2 = $sum2 + $k;
 }
 echo $sum2;
@@ -38,11 +40,62 @@ echo "<p></p>";
 $start3 = -99;
 $stop3 = 99;
 $product = 1;
-for ($x=$start3; $x <$stop3 ; $x++) { 
-	$product = $product * $x;
+for ($x=$start3; $x <=$stop3 ; $x++) { 
+	$product *= $x;
 }
-echo round($product);
+echo $product;
 
+//while loops - same exercise - slide 19
+//sum of all even numbers from 0 to 1200
+echo "<p></p>";
+$i = 0;
+$end = 1200;
+$sum = 0;
 
+while ( $i <= $end) {
+ 	if ($i %2 == 0) {
+ 		$sum+= $i;
+ 	 }
+ 	 $i++;
+ } 
+echo $sum;
+echo "<p></p>";
+
+//sum of all odd numbers from 20 to 800
+$i =  20 ;
+$stop = 800;
+$sum = 0;
+
+while ( $i  <=$stop) {
+	if ($i %2 != 0) {
+		$sum += $i;
+	}
+	$i++;
+}
+echo $sum;
+echo "<p></p>";
+//sum of all numbers from 1000 to 1
+
+$i = 1000;
+$stop = 1;
+$sum = 0;
+
+while ( $i >= $stop) {
+	$sum += $i;
+	$i--;
+}
+echo $sum;	
+echo "<p></p>";
+
+//the product of the numbers -99 and +99
+$start = -99;
+$stop = 99;
+$product = 1;
+
+while ($start <= $stop) {
+	$product = $product * $start;
+	$start++;
+}
+echo $product;
 
 ?>
