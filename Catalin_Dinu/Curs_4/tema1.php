12eabe75deea852a80920cb7e01f96a47f201f56
catalinradudinu <catalinradudinu@gmail.com>
2015-11-04 13:36:47 +0200

    2 - remove Jane`s ticket

--- Catalin_Dinu/Curs_4/tema1.php
+++ Catalin_Dinu/Curs_4/tema1.php
@@ -1,9 +1,16 @@
 <?php
 $loto_players = array(
-					array("first_name" => "John", "last_name" => "Doe", "loto_ticket" => array(11, 3, 22, 17,36, 40)),
-					array("first_name" => "Jane", "last_name" => "Foo", "loto_ticket" => array(32, 6, 18, 21,13, 42)),
-					array("first_name" => "Elvis", "last_name" => "Peanutbutter", "loto_ticket" => array(21, 7, 17, 35, 5, 44))
+					array("first_name" => "John",
+						  "last_name" => "Doe",
+						  "loto_ticket" => array(11, 3, 22, 17, 36, 40)),
+					array("first_name" => "Jane",
+						  "last_name" => "Foo",
+						  "loto_ticket" => array(32, 6, 18, 21, 13, 42)),
+					array("first_name" => "Elvis",
+						  "last_name" => "Peanutbutter",
+						  "loto_ticket" => array(21, 7, 17, 35, 5, 44))
 					);
 $loto_players[2]["loto_ticket"][6] = 13;
-print_r($loto_players);
+$loto_players[1]["loto_ticket"] = array();
+//unset($loto_players[1]["loto_ticket"]);
 ?>
