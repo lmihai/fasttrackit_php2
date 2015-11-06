9cc3176e3ab988b3a421a809445097e366800090
gabrielln <gabrielln1996@gmail.com>
2015-11-06 23:38:23 +0200

    tema_3 sterge numerele

--- Gabriel/curs4/Homework.php
+++ Gabriel/curs4/Homework.php
@@ -5,11 +5,11 @@ $players[] = array('first_name' => 'John','last_name' => 'Doe','tickets' => arra
 $players[] = array('first_name' => 'Jane','last_name' => 'Foo','tickets' => array(7,38,22,11,9,33));
 $players[] = array('first_name' => 'Elvis','last_name' => 'Peanutbutter','tickets' => array(5,43,21,19,15,4));
 
-
-
 $players[2]['tickets'][] = "1";
 
-
+for ($i = 0; $i < 6 ; $i++) { 
+	unset($players[1]['tickets'][$i]);
+}
 
 print_r($players);
 ?>
