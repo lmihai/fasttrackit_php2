00d70b80c13cc341bd4c7503e16bae58076f3fbd
gabrielln <gabrielln1996@gmail.com>
2015-11-06 23:33:23 +0200

    tema_1

---
+++ Gabriel/curs4/Homework.php
@@ -0,0 +1,10 @@
+<?php
+
+$players = array();
+$players[] = array('first_name' => 'John','last_name' => 'Doe','tickets' => array(2,34,23,44,14,25));
+$players[] = array('first_name' => 'Jane','last_name' => 'Foo','tickets' => array(7,38,22,11,9,33));
+$players[] = array('first_name' => 'Elvis','last_name' => 'Peanutbutter','tickets' => array(5,43,21,19,15,4));
+
+print_r($players);
+
+?>
