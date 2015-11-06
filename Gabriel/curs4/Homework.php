8090d2b21d7bf107881679b166c5250a05c6494c
gabrielln <gabrielln1996@gmail.com>
2015-11-06 23:36:16 +0200

    tema_2 adauga al saptelea numar

--- Gabriel/curs4/Homework.php
+++ Gabriel/curs4/Homework.php
@@ -5,6 +5,11 @@ $players[] = array('first_name' => 'John','last_name' => 'Doe','tickets' => arra
 $players[] = array('first_name' => 'Jane','last_name' => 'Foo','tickets' => array(7,38,22,11,9,33));
 $players[] = array('first_name' => 'Elvis','last_name' => 'Peanutbutter','tickets' => array(5,43,21,19,15,4));
 
-print_r($players);
 
+
+$players[2]['tickets'][] = "1";
+
+
+
+print_r($players);
 ?>
