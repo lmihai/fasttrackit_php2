1531b07c4dcb59be2c6f8b313b06a69f02371bd4
FavianIoel <favianioel@gmail.com>
2015-11-07 01:06:45 +0200

    Task 5 Display the biggest number from all tickets .

--- FavianIoel/Homework/course_4.php
+++ FavianIoel/Homework/course_4.php
@@ -12,7 +12,7 @@ name; last name and the ticket with 6 numbers.*/
 $friends = array();
 $friends[1] = array('first_name'=>'John', 'last_name'=>'Doe', 'ticket' => array(1,3,21,4,54,32));
 $friends[2] = array('first_name'=>'Jane', 'last_name'=>'Foo','ticket' => array(31,13,12,14,45,43));
-$friends[3] = array('first_name'=>'Elvis', 'last_name'=>'Peanutbutter','ticket' => array(33,11,12,24,44,54));
+$friends[3] = array('first_name'=>'Elvis', 'last_name'=>'Peanutbutter','ticket' => array(33,11,12,24,44,56));
 
 /* 2. Elvis selects a 7th number ,  add it to his 
    ticket*/
@@ -65,4 +65,21 @@ $sum = 0;
 			}
 		}
 		echo "The total sum of all numbers from all tickets: " . $sum . "<br/>";
+
+/*Display the biggest number from all tickets . */
+echo('<strong><p>Dispaly the biggest number from all tickets .</p></strong>');
+
+
+  $john_max = max($friends[1]['ticket']);
+  $elvis_max = max($friends[3]['ticket']);
+
+  if ($john_max  < $elvis_max) {
+  	echo 'the biggest number is '.$elvis_max.'. <br>' ;
+  }
+       else{
+       	echo 'the biggest number is '.$john_max.'. <br>';
+       }
+ 
+  
+  
 ?>
