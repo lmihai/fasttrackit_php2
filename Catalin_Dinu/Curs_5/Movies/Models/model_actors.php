e5e2ff1ed5ca45a8d89981618560af96267eae7b
catalinradudinu <catalinradudinu@gmail.com>
2015-11-10 10:33:37 +0200

    1.1 - add an array with actors

--- Catalin_Dinu/Curs_5/Movies/Models/model_actors.php
+++ Catalin_Dinu/Curs_5/Movies/Models/model_actors.php
@@ -1,3 +1,46 @@
 <?php
-
+$actors = array(
+			array(
+				array(
+					"actor" => "Marlon Brando",
+					"image" => "images/marlonbrando.jpg"
+					),
+				array(
+					"actor" => "Al Pacino",
+					"image" => "images/alpacino.jpg"
+					),
+				array(
+					"actor" => "James Caan",
+					"image" => "images/jamescaan.jpg"
+					)
+				),
+			array(
+				array(
+					"actor" => "Tim Roth",
+					"image" => "images/timroth.jpg"
+					),
+				array(
+					"actor" => "John Travolta",
+					"image" => "images/johntravolta.jpg"
+					),
+				array(
+					"actor" => "Samuel L. Jackson",
+					"image" => "images/samuelljackson.jpg"
+					)
+				),
+			array(
+				array(
+					"actor" => "Roberto Benigni",
+					"image" => "images/robertobenigni.jpg"
+					),
+				array(
+					"actor" => "Nicoletta Braschi",
+					"image" => "images/nicolettabraschi.jpg"
+					),
+				array(
+					"actor" => "Giorgio Cantarini",
+					"image" => "images/giorgiocantarini.jpg"
+					)
+				)
+			);
 ?>
