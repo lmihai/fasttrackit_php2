9358e35212633b7edd2601bb2d3914f173beee78
FavianIoel <favianioel@gmail.com>
2015-11-10 16:00:38 +0200

    Actors Array for each movie .

---
+++ FavianIoel/curs_5/my_movies/Models/model_actors.php
@@ -0,0 +1,105 @@
+<?php
+
+ $actors = array();
+    $actors[1] =  array(
+					"actor" => "Marlon Brando",
+					"image" => "images/actors/godfather/marlonbrando.jpg");
+                  array(
+                    "actor" => "Al Pacino",
+                    "image" => "images/actors/godfather/alpacino.jpg");
+                  array(
+                    "actor" => "James Caan",
+                    "image" => "images/actors/godfather/jamescaan.jpg");
+    $actors[2] =  array(
+                    "actor" => "Tim Roth",
+                    "image" => "images/actors/pulp/timroth.jpg");
+                  array(
+                    "actor" => "John Travolta",
+                    "image" => "images/actors/pulp/johntravolta.jpg");
+                  array(
+                    "actor" => "Samuel L. Jackson",
+                    "image" => "images/actors/pulp/jackson.jpg");
+    $actors[3] =  array(
+                    "actor" => "Roberto Benigni",
+                    "image" => "images/actors/life/roberto.jpg");
+                  array(
+                    "actor" => "Nicoletta Braschi",
+                    "image" => "images/actors/life/nicoleta.jpg");
+                  array(
+                    "actor" => "Giorgio Cantarini",
+                    "image" => "images/actors/life/giorgio.jpg");
+    $actors[4] =  array(
+                    "actor" => "Roberto De Niro",
+                    "image" => "images/actors/goodfellas/niro.jpg");
+                  array(
+                    "actor" => "Ray Liotta",
+                    "image" => "images/actors/goodfellas/ray.jpg");
+                  array(
+                    "actor" => "Joe Pesci",
+                    "image" => "images/actors/goodfellas/joe.jpg");
+
+    $actors[5] = array(
+                    "actor" => "Tim Robbins",
+                    "image" => "images/actors/redemption/tim.jpg");
+                 array(
+                    "actor" => "Morgan Freeman",
+                    "image" => "images/actors/redemption/morgan.jpg");
+                 array(
+                    "actor" => "Bob Gunton",
+                    "image" => "images/actors/redemption/bob.jpg");
+    $actors[6] =  array(
+                    "actor" => "Christian Bale",
+                    "image" => "images/actors/batman/bale.jpg");
+                 array(
+                    "actor" => "Health Ledger",
+                    "image" => "images/actors/batman/joker.jpg");
+                 array(
+                    "actor" => "Aaron Eckhart",
+                    "image" => "images/actors/batman/aaron.jpg");
+    $actors[7] =  array(
+                    "actor" => "Martin Balsam",
+                    "image" => "images/actors/angry/martin.jpg");
+                 array(
+                    "actor" => "John Fiedler",
+                    "image" => "images/actors/angry/john.jpg");
+                 array(
+                    "actor" => "Lee J. Cobb",
+                    "image" => "images/actors/angry/cobb.jpg");
+    $actors[8] = array(
+                    "actor" => "Liam Neeson",
+                    "image" => "images/actors/list/liam.jpg");
+                 array(
+                    "actor" => "Ben Kingsley",
+                    "image" => "images/actors/list/ben.jpg");
+                 array(
+                    "actor" => "Ralph Fiennes",
+                    "image" => "images/actors/list/ralph.jpg");
+    $actors[9] =  array(
+                    "actor" => "Edward Norton",
+                    "image" => "images/actors/fightclub/norton.jpg");
+                  array(
+                    "actor" => "Brad Pitt",
+                    "image" => "images/actors/fightclub/pitt.jpg");
+                  array(
+                    "actor" => "Helena Bonham Carter",
+                    "image" => "images/actors/fightclub/helena.jpg");
+    $actors[10] =  array(
+                    "actor" => "Mark Hamill",
+                    "image" => "images/actors/starwars/mark.jpg");
+                   array(
+                    "actor" => "Harrison Ford",
+                    "image" => "images/actors/starwars/ford.jpg");
+                   array(
+                    "actor" => "Carrie Fisher",
+                    "image" => "images/actors/starwars/carrie.jpg");
+    $actors[11] =  array(
+                    "actor" => "Leonardo DiCaprio",
+                    "image" => "images/actors/inception/leonardo.jpg");
+                   array(
+                    "actor" => "Joseph Gordon-Levitt",
+                    "image" => "images/actors/incemption/levit.jpg");
+                   array(
+                    "actor" => "Ellen Page",
+                    "image" => "images/actors/incemption/page.jpg");
+
+?>
