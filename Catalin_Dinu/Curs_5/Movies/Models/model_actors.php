55730fe4c7da71b69e4a1e800d6d830abbebc75d
catalinradudinu <catalinradudinu@gmail.com>
2015-11-11 12:55:15 +0200

    modify the array actor`s details

--- Catalin_Dinu/Curs_5/Movies/Models/model_actors.php
+++ Catalin_Dinu/Curs_5/Movies/Models/model_actors.php
@@ -1,46 +1,64 @@
 <?php
 $actors = array(
-			array(
-				array(
-					"actor" => "Marlon Brando",
-					"image" => "images/marlonbrando.jpg"
-					),
-				array(
-					"actor" => "Al Pacino",
-					"image" => "images/alpacino.jpg"
-					),
-				array(
-					"actor" => "James Caan",
-					"image" => "images/jamescaan.jpg"
-					)
-				),
-			array(
-				array(
-					"actor" => "Tim Roth",
-					"image" => "images/timroth.jpg"
-					),
-				array(
-					"actor" => "John Travolta",
-					"image" => "images/johntravolta.jpg"
-					),
-				array(
-					"actor" => "Samuel L. Jackson",
-					"image" => "images/samuelljackson.jpg"
-					)
-				),
-			array(
-				array(
-					"actor" => "Roberto Benigni",
-					"image" => "images/robertobenigni.jpg"
-					),
-				array(
-					"actor" => "Nicoletta Braschi",
-					"image" => "images/nicolettabraschi.jpg"
-					),
-				array(
-					"actor" => "Giorgio Cantarini",
-					"image" => "images/giorgiocantarini.jpg"
-					)
-				)
-			);
+					array(
+						"actor" => "Marlon Brando",
+						"image" => "images/marlonbrando.jpg"
+						),
+					array(
+						"actor" => "Al Pacino",
+						"image" => "images/alpacino.jpg"
+						),
+					array(
+						"actor" => "James Caan",
+						"image" => "images/jamescaan.jpg"
+						),
+					array(
+						"actor" => "Tim Roth",
+						"image" => "images/timroth.jpg"
+						),
+					array(
+						"actor" => "John Travolta",
+						"image" => "images/johntravolta.jpg"
+						),
+					array(
+						"actor" => "Samuel L. Jackson",
+						"image" => "images/samuelljackson.jpg"
+						),
+					array(
+						"actor" => "Roberto Benigni",
+						"image" => "images/robertobenigni.jpg"
+						),
+					array(
+						"actor" => "Nicoletta Braschi",
+						"image" => "images/nicolettabraschi.jpg"
+						),
+					array(
+						"actor" => "Giorgio Cantarini",
+						"image" => "images/giorgiocantarini.jpg"
+						),
+					array(
+						"actor" => "Chris O'Donnell",
+						"image" => "images/chrisodonnell.jpg"
+						),
+					array(
+						"actor" => "James Rebhorn",
+						"image" => "images/jamesrebhorn.jpg"
+						)
+					);
+
+/* $actors = array(
+0 =>  ARRAY(
+	ACTOR => 
+	IMAGE =>
+	),
+1 =>
+);
+
+$MOVIES_ACTORS = ARRAY(
+	MOVIE_KEY => ARRAY(1, 7, 9)
+	)
+
+
+	$ACTOES[7]
+*/
 ?>
