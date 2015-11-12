015f4ad5e2b3272d7d121cd80550442e4b23f05b
rushoratiu <rushoratiu3@gmail.com>
2015-11-12 09:55:32 +0200

    i added the actors. - Soz for few extra commits, i gotta get rid of them

---
+++ Horatiu/Site-movies/models/model_actors.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$actors = array();
+$actors[1] = array('name' => 'Marlon Brando', 'photo' => 'images/actors/marlon.jpg'), 
+			 array('name' => 'Al Pacino', 'photo' => 'images/actors/pacino.jpg'),
+			 array('name' => 'James Caan', 'photo' => 'images/actors/james.jpg');
+$actors[2] = array('name' => 'Tim Roth', 'photo' => 'images/actors/roth.jpg'),
+			 array('name' => 'John Travolta', 'photo' => 'images/actors/travolta.jpg'),
+			 array('name' => 'Samuel L. Jackson', 'photo' => 'images/actors/jackson.jpg');
+$actors[3] = array('name' => 'Roberto Benigni', 'photo' => 'images/actors/benigni.jpg'),
+			 array('name' => 'Nicoletta Braschi', 'photo' => 'images/actors/nicoletta.jpg'),
+			 array('name' => 'Giorgio Cantarini', 'photo' => 'images/actors/giorgio.jpg');
+			 
+
+
+
+
+
+?>
---
+++ Horatiu/Site-sports/view/view_sport_details.tpl.php
@@ -0,0 +1,3 @@
+<h1> <?php echo $sport_details['type']; ?> </h1>
+<h2> <?php echo $sport_details['invented'] ?> </h2>
+<img src="<?php echo $sport_details['poster']; ?>" >
