2a570db93749873027d7f1118eb928be1717cf79
rushoratiu <rushoratiu3@gmail.com>
2015-11-12 17:38:03 +0200

    style + actors + etc..

--- Horatiu/Site-movies/models/model_actors.php
+++ Horatiu/Site-movies/models/model_actors.php
@@ -2,19 +2,62 @@
 
 
 $actors = array();
-$actors[1] = array('name' => 'Marlon Brando', 'photo' => 'images/actors/marlon.jpg'), 
-			 array('name' => 'Al Pacino', 'photo' => 'images/actors/pacino.jpg'),
-			 array('name' => 'James Caan', 'photo' => 'images/actors/james.jpg');
-$actors[2] = array('name' => 'Tim Roth', 'photo' => 'images/actors/roth.jpg'),
-			 array('name' => 'John Travolta', 'photo' => 'images/actors/travolta.jpg'),
-			 array('name' => 'Samuel L. Jackson', 'photo' => 'images/actors/jackson.jpg');
-$actors[3] = array('name' => 'Roberto Benigni', 'photo' => 'images/actors/benigni.jpg'),
-			 array('name' => 'Nicoletta Braschi', 'photo' => 'images/actors/nicoletta.jpg'),
-			 array('name' => 'Giorgio Cantarini', 'photo' => 'images/actors/giorgio.jpg');
-			 
+$actors[1] = array('name1' => 'Marlon Brando', 'photo1' => 'images/actors/marlon.jpg',
+			 'name2' => 'Al Pacino', 'photo2' => 'images/actors/pacino.jpg',
+			 'name3' => 'James Caan', 'photo3' => 'images/actors/james.jpg');
+$actors[2] = array('name1' => 'Tim Roth', 'photo1' => 'images/actors/roth.jpg',
+			 'name2' => 'John Travolta', 'photo2' => 'images/actors/travolta.jpg',
+			 'name3' => 'Samuel L. Jackson', 'photo3' => 'images/actors/jackson.jpg');
+$actors[3] = array('name1' => 'Roberto Benigni', 'photo1' => 'images/actors/benigni.jpg',
+			 'name2' => 'Nicoletta Braschi', 'photo2' => 'images/actors/nicoletta.jpg',
+			 'name3' => 'Giorgio Cantarini', 'photo3' => 'images/actors/giorgio.jpg');
 
+/*
 
+$actors = array(
+			array(
+				array('name1' => 'Marlon Brando', 'photo1' => 'images/actors/marlon.jpg'),
+				array('name2' => 'Al Pacino', 'photo2' => 'images/actors/pacino.jpg'),
+				array('name3' => 'James Caan', 'photo3' => 'images/actors/james.jpg')
+				),
+			array(
+				array('name1' => 'Tim Roth', 'photo1' => 'images/actors/roth.jpg'),
+				array('name2' => 'John Travolta', 'photo2' => 'images/actors/travolta.jpg'),
+				array('name3' => 'Samuel L. Jackson', 'photo3' => 'images/actors/jackson.jpg')
+				),
+			array(
+				array('name1' => 'Roberto Benigni', 'photo1' => 'images/actors/benigni.jpg'),
+				array('name2' => 'Nicoletta Braschi', 'photo2' => 'images/actors/nicoletta.jpg'),
+				array('name3' => 'Giorgio Cantarini', 'photo3' => 'images/actors/giorgio.jpg')
+			));
 
+			 $actors = array();
+    $actors[1] =  array(
+					         "actor1" => "Marlon Brando",
+					         "image1" => "images/actors/godfather/marlonbrado.jpg",
+                  
+                    "actor2" => "Al Pacino",
+                    "image2" => "images/actors/godfather/alpacino.jpg",
+                  
+                    "actor3" => "James Caan",
+                    "image3" => "images/actors/godfather/jamescaan.jpg");
+    $actors[2] =  array(
+                    "actor1" => "Tim Roth",
+                    "image1" => "images/actors/pulp/timroth.jpg",
+                  
+                    "actor2" => "John Travolta",
+                    "image2" => "images/actors/pulp/johntravolta.jpg",
+                  
+                    "actor3" => "Samuel L. Jackson",
+                    "image3" => "images/actors/pulp/jackson.jpg");
+    $actors[3] =  array(
+                    "actor1" => "Roberto Benigni",
+                    "image1" => "images/actors/life/roberto.jpg",
+                  
+                    "actor2" => "Nicoletta Braschi",
+                    "image2" => "images/actors/life/nicoleta.jpg",
+                  
+                    "actor3" => "Giorgio Cantarini",
+                    "image3" => "images/actors/life/giorgio.jpg");
+*/
 
-
-?>
