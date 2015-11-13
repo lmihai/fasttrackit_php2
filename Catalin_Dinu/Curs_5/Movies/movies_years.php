925cb18e5b345601335c6456c6466a4c546b503f
catalinradudinu <catalinradudinu@gmail.com>
2015-11-13 12:13:50 +0200

    display movie years

--- Catalin_Dinu/Curs_5/Movies/movies_years.php
+++ Catalin_Dinu/Curs_5/Movies/movies_years.php
@@ -4,7 +4,7 @@ include("models/model_movies.php");
 for ($i=0; $i < count($movies); $i++) {
 	$movie_title = $movies[$i]["title"];
 		if (isset($_GET["year"])) {		
-			 if ($_GET["year"] == $movies[$i]["year"]) {
+			 if ($_GET["year"] === $movies[$i]["year"]) {
 			 	echo ('<li><a href="movies_details.php?movie=' . $i . '">' . $movie_title . '</a></li><br>');
 				}
 			}
