4c21e809a4f6b753c1dd31f33f4c53c397a52f67
catalinradudinu <catalinradudinu@gmail.com>
2015-11-13 09:45:53 +0200

    display movie years, genres and cast

--- Catalin_Dinu/Curs_5/Movies/Views/view_movies_details.tpl.php
+++ Catalin_Dinu/Curs_5/Movies/Views/view_movies_details.tpl.php
@@ -1,13 +1,31 @@
 <link rel="stylesheet" type="text/css" href="views/style.css">
 <h1> <?php echo($movie_details["title"]); ?> </h1>
-<h2> <?php echo($movie_details["year"]); ?> </h2>
+<h2> <?php echo '<a href="movies_years.php?year=' . $movie_details["year"] .'">' . $movie_details["year"]; '</a>' ?> </h2>
+<h3>Genres:
+<?php
+	foreach ($movie_details["genres"] as $movie_detail_genres) {
+		echo'<a href="movies_genres.php?genre=' . $movie_detail_genres .'">' . $movie_detail_genres . "</a>" . " ";
+			}
+?>
+</h3>
 <img src="<?php echo($movie_details["poster"]); ?> ">
 <h3>Cast</h3>
 <?php
-for ($i=0; $i < count($actors[0]); $i++) {
-		$actor_names = $actors[$i][0]["actor"];
-		$actor_image = $actors[$i][0]["image"];
-		echo '<img src="' . $actor_image . '"style="width:40px;">' . $actor_names . "<br>";
+function cast_actors() {
+	global $movie_details;
+foreach ($movie_details["actors"] as $movie_detail_cast) {
+	foreach ($movie_detail_cast as $key => $movie_detail_actors) {
+			if ($key == 'image') {
+				echo  '<img src="'. $movie_detail_actors .'" style="width:45px;"> ';
+			}
+			else {
+				echo  $movie_detail_actors;
+			}
 		}
+		echo  "<br>";
+	}
+}
+cast_actors();
 ?>
 <p><a href="movies.php">Back to movies</a></p>
+
