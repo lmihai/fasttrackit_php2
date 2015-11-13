2e48c96e859b9a8ff82afb8ce9ca70aa4f4f4bc4
catalinradudinu <catalinradudinu@gmail.com>
2015-11-13 09:48:44 +0200

    implement the page to display all movies that appeared in a specific year

---
+++ Catalin_Dinu/Curs_5/Movies/movies_years.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" type="text/css" href="views/style.css">
+<?php
+include("models/model_movies.php");
+for ($i=0; $i < count($movies); $i++) {
+	$movie_title = $movies[$i]["title"];
+		if (isset($_GET["year"])) {		
+			 if ($_GET["year"] == $movies[$i]["year"]) {
+			 	echo ('<li><a href="movies_details.php?movie=' . $i . '">' . $movie_title . '</a></li><br>');
+				}
+			}
+}
+?>
