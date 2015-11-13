8e015a30cde5295300d1e4dd879c19e383b21c71
Teo Roman <teo_romann@yahoo.com>
2015-11-13 21:50:35 +0200

    Curs 5 PHP: Mymovies project

---
+++ Teodora/curs5/Mymovies/movies_details.php
@@ -0,0 +1,13 @@
+<?php
+
+	include('models/model_movies.php');
+
+	if(isset($_GET['movie'])) {
+			//$movie_position_in_array = $_GET['movie'];
+			//do something about that movie 
+			//print($movie_position_in_array);
+
+			//instead of accessing the array directly from the controller file, let's use the fnew function
+			$movies_details = get_movie_details($_GET['movie']);
+			include('views/view_movies_details.tpl.php');
+		}	
