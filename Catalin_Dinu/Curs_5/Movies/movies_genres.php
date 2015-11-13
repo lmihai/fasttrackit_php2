1087fe662cca4dabb1678335274e73df7e7aa595
catalinradudinu <catalinradudinu@gmail.com>
2015-11-13 12:13:30 +0200

    display movie genres

--- Catalin_Dinu/Curs_5/Movies/movies_genres.php
+++ Catalin_Dinu/Curs_5/Movies/movies_genres.php
@@ -4,12 +4,15 @@ include("models/model_movies_genres.php");
 include("models/model_movies.php");
 for ($i=0; $i < count($movies); $i++) {
 	$movie_title = $movies[$i]["title"];
-		if (isset($_GET["genre"])) {		
-			 if ($_GET["genre"] == $genres[$i]) {
-			 	echo ('<li><a href="movies_details.php?movie=' . $i . '">' . $movie_title . '</a></li><br>');
+		if (isset($_GET["genre"])) {
+			foreach ($movies[$i]["genres"] as $genre) {		
+			 	if ($_GET["genre"] === $genre) {
+			 		echo ('<li><a href="movies_details.php?movie=' . $i . '">' . $movie_title . '</a></li><br>');
 				}
 			}
+	}
 }
 ?>
 
 
+	
