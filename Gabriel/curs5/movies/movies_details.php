c52cec245d16e760c57845226b2f25ec387f30c9
gabrielln <gabrielln1996@gmail.com>
2015-11-14 09:50:00 +0200

    curs_5 cu functii

--- Gabriel/curs5/movies/movies_details.php
+++ Gabriel/curs5/movies/movies_details.php
@@ -1,14 +1,18 @@
-<?php
+
+	
+	<?php
+
 
 
 	include('models/model_movies.php');
+	include('models/model_actors.php');
 	if (isset( $_GET['movie'])) {
-		$movie_position_in_array = $_GET['movie'];
-		$movie_details = $movies[$movie_position_in_array];
+		$movie_details = get_movie_details($_GET['movie']);
 		include('views/view_movie_details.tpl.php');
-		/*print('<h1>' . $movie_details['title'] . '</h1>');
-		print('<h2>' . $movie_details['year'] . '</h2>');
-		print('<img src="' . $movie_details['poster'] . '">');
-		print('<p><a href="movies.php?">BACK</a></p>');
-*/
+		$actor_details = get_actor_details($_GET['movie']);
+		include('views/view_actor_details.tpl.php');
+		
 	}
+	?>
+
+
