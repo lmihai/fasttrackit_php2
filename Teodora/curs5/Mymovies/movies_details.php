60a38693f6f6e8f210e5405894621950dfbc7d70
Teo Roman <teo_romann@yahoo.com>
2015-11-14 23:37:14 +0200

    Homework curs5 PHP; included view actors and function get_actors_details in controller

--- Teodora/curs5/Mymovies/movies_details.php
+++ Teodora/curs5/Mymovies/movies_details.php
@@ -1,6 +1,7 @@
 <?php
 
 	include('models/model_movies.php');
+	include('models/model_actors.php');
 
 	if(isset($_GET['movie'])) {
 			//$movie_position_in_array = $_GET['movie'];
@@ -10,4 +11,6 @@
 			//instead of accessing the array directly from the controller file, let's use the fnew function
 			$movies_details = get_movie_details($_GET['movie']);
 			include('views/view_movies_details.tpl.php');
+			$actor_details = get_actor_details($_GET['movie']);
+			include('views/view_actor_details.tpl.php');
 		}	
