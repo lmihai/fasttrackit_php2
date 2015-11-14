2d2cfdc24211c14362d68be5dc0765f0bf0a1916
cmrazvan <costeamihairazvan@gmail.com>
2015-11-14 11:07:26 +0200

    dadaa

---
+++ CMRazvan/Curs 6/index.php
@@ -0,0 +1,28 @@
+<?php
+print("Curs 6");
+
+?>
+
+
+
+<html>
+<head>
+	<title>A Bassic HTML FORM</title>
+</head>
+<body>
+
+		<form name="form1" method="post" action="">
+				<textarea name="comment" form="usrform">Am reusit</textare>
+				<select>
+				  <option value="Yes">Yes</option>
+				  <option value="No">No</option>
+				  <option value="Maybe">Maybe</option>
+				</select>
+			<INPUT TYPE="TEXT" Name="Razvan" VALUE="username">
+			<INPUT TYPE="Submit" Name="Submit1" VALUE="Login">
+		</form>
+
+
+
+</body>
+</html>
