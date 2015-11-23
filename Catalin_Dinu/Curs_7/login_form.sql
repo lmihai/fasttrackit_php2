950dd0cfed127fa904743429676e3efb9fd0de20
catalinradudinu <catalinradudinu@gmail.com>
2015-11-23 12:56:27 +0200

    Homework 7; 1 - Create a table; 2 - Populate it with data

---
+++ Catalin_Dinu/Curs_7/login_form.sql
@@ -0,0 +1,66 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 23, 2015 at 11:53 AM
+-- Server version: 5.6.25
+-- PHP Version: 5.6.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `homework_7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login form`
+--
+
+CREATE TABLE IF NOT EXISTS `login form` (
+  `id` int(11) NOT NULL,
+  `username` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `login form`
+--
+
+INSERT INTO `login form` (`id`, `username`, `password`) VALUES
+(1, 'simonahalep', 'b3370292310166dcb6394232511aba6a'),
+(2, 'irinabegu', 'ca64cb634448891949d65c1133e58e45'),
+(3, 'horiatecau', 'fed62fd7c51cd3df341fc0d33c6c8013'),
+(4, 'florinmergea', 'd318c6516fa745056049e5c1c8c19b91');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login form`
+--
+ALTER TABLE `login form`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login form`
+--
+ALTER TABLE `login form`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
