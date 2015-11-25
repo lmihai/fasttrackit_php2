011560de696d9764a6123c2afa0af303128834cb
catalinradudinu <catalinradudinu@gmail.com>
2015-11-25 11:17:03 +0200

    3.2 -  check if user exists in database and the password matches the one in the db

--- Catalin_Dinu/Curs_7/login.php
+++ Catalin_Dinu/Curs_7/login.php
@@ -1,3 +1,46 @@
+<?php
+
+$server = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "homework_7";
+
+$db_conn = mysql_connect($server, $db_user, $db_pass);
+
+
+if (!$db_conn) {
+    echo "Unable to connect to DB: " . mysql_error();
+    exit;
+}
+
+if (!mysql_select_db("homework_7")) {
+    echo "Unable to select homework_7: " . mysql_error();
+    exit;
+}
+if(isset($_POST["username"]) && isset($_POST["password"])) {
+	$user = $_POST["username"];
+	$pass = MD5($_POST["password"]);
+
+ $sql = 'SELECT * FROM login_form WHERE username = "' . $user . '" and password = "' . $pass . '"';
+	$result = mysql_query($sql, $db_conn) or die ( "Error : ". mysql_error() );
+	$row = mysql_fetch_array($result);
+	if($row["username"]==$user && $row["password"]==$pass) {
+    	echo"Excelent";
+		}
+	else {
+    	echo"Sorry";
+	}
+
+}
+
+/*
+if (isset($_SESSION["username"])) {
+	header ('Locaction: http://localhost/fasttrackit_php2/Catalin_Dinu/Curs_7/user.php');
+}
+*/
+
+?>
+
 <html>
 	<head>
 		<title>LOGIN</title>
@@ -5,7 +48,7 @@
 	<body>
 		<form name="form" method="post" action="login.php">
 				<input type = "text" name="username" value = ""> username<br><br>
-				<input type = "text" name="password" value = ""> password<br><br>
+				<input type = "password" name="password" value = ""> password<br><br>
 				<input type="submit" value="Login">
 		</form>
 	</body>
