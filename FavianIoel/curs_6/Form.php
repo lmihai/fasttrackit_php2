ea2803a5f83aa487cfb6a8a7e46ec2af3e65dc38
FavianIoel <favianioel@gmail.com>
2015-12-14 13:10:41 +0200

    form method get course 6

---
+++ FavianIoel/curs_6/Form.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+	<title>Login Form</title>
+</head>
+<body>
+
+		<form name="form1" method="get" action="">
+			<input type ="text" value="username">
+			<input typee = "submit" name = "submit1" value = "login">
+		
+		</form>
+
+
+
+</body>
+</html>
