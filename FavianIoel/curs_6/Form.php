f72673fb1af2067b59531c0edf93d3586be7a73b
FavianIoel <favianioel@gmail.com>
2015-12-14 13:42:21 +0200

    form submit button

--- FavianIoel/curs_6/Form.php
+++ FavianIoel/curs_6/Form.php
@@ -3,19 +3,33 @@
 	<title>Login Form</title>
 </head>
 <body>
-
+   <p>Form prin metoda "get" si actiunea catre alt fisier!</p>
 		<form name="form1" method="get" action="formdestination.php">
 			<input type ="text" value="username">
 			<input type = "submit" name = "submit1" value = "login">
 		
 		</form>
-
+        <hr>
+        <p>Form cu metoda post si actiunea catre acest fisier!</p>
 		<form name="form1" method="post" action="form.php">
 			<input type ="text" value="username">
 			<input type = "submit" name = "submit1" value = "login">
 		
 		</form>
+		<br>
+		<br>
+        <hr>
+        <p>form fara  type = "submit"</p>
+        <form name="form1" method="get" action="">
+        	<input type="text" value="username">
+        </form>
 
+        <hr>
+        <p>form cu type="submit" !</p>
+        <form name ="form1" method="get" action="">
+        	<input type ="text" value="username">
+        	<input type = "submit" name="submit1" value="enter!">
+        </form>
 
 
 
