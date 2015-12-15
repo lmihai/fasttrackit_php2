5b5051527b2d24c460bd58f5942acf400d2a02ad
FavianIoel <favianioel@gmail.com>
2015-12-15 19:38:57 +0200

    hello , hello again , goodbye COOKIE with include();

---
+++ FavianIoel/curs_7/cookies/helloag.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+	<link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>HELLO AGAIN</title>
+</head>
+<body>
+	<br>
+	<br>
+ <center>
+<h1>HELLO AGAIN!!!</h1>
+   </center>
+</body>
+</html>
