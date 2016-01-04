a44a2f92dea5fc939fcc44508819748da6c5acd4
FavianIoel <favianioel@gmail.com>
2016-01-04 15:52:42 +0200

    querry table content

--- FavianIoel/Homework/course_8/conn.php
+++ FavianIoel/Homework/course_8/conn.php
@@ -5,11 +5,40 @@ $password = "";
 $dbname = "school";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password,$dbname);
+$conn = mysqli_connect($servername, $username, $password,$dbname);
 
 // Check connection
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+	die("Connection failed: " . mysqli_connect_error());
 }
-echo "<p>Connected successfully</p>";
+
+// Attempt select query execution
+$sql = "SELECT * FROM courses";
+if ($result = mysqli_query($conn, $sql)) {
+	if (mysqli_num_rows($result)>0) {
+		echo "<table>";
+		echo "<th>id</th>";
+		echo "<th>CourseName</th>";
+		echo "<th>Trainer</th>";
+		echo "</tr>";
+		while ($row = mysqli_fetch_array($result)) {
+			echo "<tr>";
+			echo "<td>" . $row['ID'] . "</td>";
+			echo "<td>" . $row['CourseName'] . "</td>";
+			echo "<td>" . $row['Trainer'] . "</td>";
+			echo "</tr>";
+		}
+		echo "</table>";
+		
+   	//Close result set
+		mysqli_free_result($result);
+	} else {
+		echo "No records matching your query were found .";
+	}
+}else {
+	echo "ERROR: Could not able to execute $sql." . mysqli_close($conn);
+}
+
+//close conection
+mysqli_close($conn);
 ?>
--- FavianIoel/Homework/course_8/index.php
+++ FavianIoel/Homework/course_8/index.php
@@ -4,6 +4,11 @@
     <link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-<?php include 'conn.php'; ?>    
+    <center>
+        <hr>
+        <h1>COURSES MANAGEMENT PAGE</h1>
+        <hr><br>
+        <?php include 'conn.php'; ?>
+    </center>    
 </body>
 </html>
