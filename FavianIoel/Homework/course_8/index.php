362875cc7f7637c93234421c75ed8b5a122f09a9
FavianIoel <favianioel@gmail.com>
2016-01-04 15:03:23 +0200

    hmw

---
+++ FavianIoel/Homework/course_8/index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+    <title>Courses Management Page</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<?php include 'conn.php'; ?>    
+</body>
+</html>
